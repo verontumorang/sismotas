898f4da0685ecb592db6d1c4422d3788682936c4
verontumorang <veronmanasse@gmail.com>
2023-04-11 14:17:10 +0700

    p
    hallo

--- storage/framework/views/61859e131e6349d5fca026f7338b5391.php
+++ storage/framework/views/61859e131e6349d5fca026f7338b5391.php
@@ -4,4 +4,4 @@
 <li class="nav-item"><a class="nav-link" href="<?php echo e(backpack_url('teacher')); ?>"><i class="nav-icon la la-user"></i> Teachers</a></li>
 <li class="nav-item"><a class="nav-link" href="<?php echo e(backpack_url('course')); ?>"><i class="nav-icon la la-school"></i> Courses</a></li>
 <li class="nav-item"><a class="nav-link" href="<?php echo e(backpack_url('teacher-class')); ?>"><i class="nav-icon la la-school"></i> Teacher Class</a></li>
-<li class="nav-item"><a class="nav-link" href="<?php echo e(backpack_url('student')); ?>"><i class="nav-icon la la-question"></i> Students</a></li><?php /**PATH D:\projects\sismotas\resources\views/vendor/backpack/base/inc/sidebar_content.blade.php ENDPATH**/ ?>
+<li class="nav-item"><a class="nav-link" href="<?php echo e(backpack_url('student')); ?>"><i class="nav-icon la la-user"></i> Students</a></li><?php /**PATH D:\projects\sismotas\resources\views/vendor/backpack/base/inc/sidebar_content.blade.php ENDPATH**/ ?>

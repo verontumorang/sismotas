3f05b4cf27ba64b35d6af4d89873032a5c488111
verontumorang <veronmanasse@gmail.com>
2023-04-11 14:29:44 +0700

    dah tuh bang parents

--- app/Models/Parents.php
+++ app/Models/Parents.php
@@ -2,14 +2,16 @@
 
 namespace App\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class Parents extends Model
 {
+    use CrudTrait;
     use HasFactory;
-    use HasUuid;
+    use HasUuids;
 
     protected $table = 'parents';
     protected $primaryKey = 'uuid';
